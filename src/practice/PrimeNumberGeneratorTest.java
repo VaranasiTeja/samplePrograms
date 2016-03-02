639724938ed816aa5cd28bd26b7c74fdc75b071f
Teja <teja.varanasi@tivamo.com>
2016-03-02 17:43:11 +0530

    adding condition for not to check all even numbers

--- src/practice/PrimeNumberGeneratorTest.java
+++ src/practice/PrimeNumberGeneratorTest.java
@@ -15,12 +15,13 @@ public class PrimeNumberGeneratorTest {
 	@Test
 	public void testPositiveNumbersInRange() {
 		ExecutorService threadPool = Executors.newFixedThreadPool(4);
+		System.out.println("-------"+primeNumberGenerator.generatePrimeNumbersInRange(10, 30));
 
 		Runnable test1 = new Runnable() {
 			@Override
 			public void run() {
 				System.out.println("1");
-				System.out.println(primeNumberGenerator.generatePrimeNumbersInRange(10, 15));
+				System.out.println("TEst1 ResULT" + primeNumberGenerator.generatePrimeNumbersInRange(10, 15));
 				Assert.assertTrue(false);
 				Assert.assertEquals(primeNumberGenerator.generatePrimeNumbersInRange(10, 15),
 						new ArrayList<Integer>(Arrays.asList(11, 13)));
@@ -31,8 +32,9 @@ public class PrimeNumberGeneratorTest {
 			@Override
 			public void run() {
 				System.out.println("2");
+				System.out.println("TEst2 ResULT" + primeNumberGenerator.generatePrimeNumbersInRange(0, 15));
 				Assert.assertEquals(primeNumberGenerator.generatePrimeNumbersInRange(0, 15),
-						new ArrayList<Integer>(Arrays.asList(2, 3, 5, 7, 8, 11, 13)));
+						new ArrayList<Integer>(Arrays.asList(2, 3, 5, 7, 11, 13)));
 			}
 		};
 		Runnable test3 = new Runnable() {
