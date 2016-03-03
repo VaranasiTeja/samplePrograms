8e1ac50cc20894e4c08104ca9df794aa8d285ea0
Teja <teja.varanasi@tivamo.com>
2016-03-03 16:06:06 +0530

    code added for finding primes till range using optimized algorithm sieve of Eratosthenes

--- src/practice/PrimeNumberGenerator.java
+++ src/practice/PrimeNumberGenerator.java
@@ -1,34 +1,61 @@
 package practice;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class PrimeNumberGenerator {
 
-	public ArrayList<Integer> generatePrimeNumbersInRange(int startingNumber, int endingNumber) {
-		ArrayList<Integer> primeNumbers = new ArrayList<Integer>();
+	public List<Integer> generatePrimeNumbersInRange(int startingNumber, int endingNumber) {
+		List<Integer> primeNumbers = new ArrayList<Integer>();
 		if (startingNumber > endingNumber) {
 			int temp = startingNumber;
 			startingNumber = endingNumber;
 			endingNumber = temp;
 		}
-		startingNumber = startingNumber < 2 ? 2 : startingNumber;
-		if (startingNumber <= 2)
-			primeNumbers.add(0, 2);
 		for (int i = startingNumber; i < endingNumber; i++) {
-			boolean flag = true;
-			double squareRoot = Math.sqrt(i);
-			if (i % 2 == 0)
-				flag = false;
-			for (int j = 3; j <= squareRoot; j = j + 2) {
-				if (i % j == 0) {
-					flag = false;
-					break;
-				}
-			}
-			if (flag) {
+			if (isPrime(i))
 				primeNumbers.add(i);
-			}
 		}
 		return primeNumbers;
+	}
+
+	public List<Integer> generatePrimeNumbersTillEndingNumber(int endingNumber) {
+		return generatePrimeNumbersInRange(0, endingNumber);
+	}
+
+	public boolean isPrime(int num) {
+		boolean isPrime = true;
+		if (num < 2 || (num != 2 && num % 2 == 0))
+			isPrime = false;
+		double squareRoot = Math.sqrt(num);
+		for (int j = 3; j <= squareRoot; j = j + 2) {
+			if (num % j == 0) {
+				isPrime = false;
+				break;
+			}
+		}
+		return isPrime;
+	}
+
+	public List<Integer> generatePrimeNumbersBySeive(int endingNumber) {
+		List<Boolean> booleanList = new ArrayList<Boolean>(Arrays.asList(new Boolean[endingNumber]));
+		Collections.fill(booleanList, Boolean.TRUE);
+		double squareRoot = Math.sqrt(endingNumber);
+		for (int num = 2; num < squareRoot; num++) {
+			if (booleanList.get(num)) {
+				for (int j = num * num; j < endingNumber; j += (num)) {
+					booleanList.set(j, false);
+				}
+			}
+		}
+		List<Integer> primeNumbersList = new ArrayList<Integer>();
+		for (int j = 2; j < booleanList.size(); j++) {
+			if (booleanList.get(j)) {
+				primeNumbersList.add(j);
+			}
+		}
+		return primeNumbersList;
 	}
 }
--- src/practice/PrimeNumberGeneratorTest.java
+++ src/practice/PrimeNumberGeneratorTest.java
@@ -12,68 +12,111 @@ public class PrimeNumberGeneratorTest {
 
 	PrimeNumberGenerator primeNumberGenerator = new PrimeNumberGenerator();
 
+//	@Test
+//	public void testPositiveNumbersInRange() {
+//		ExecutorService threadPool = Executors.newFixedThreadPool(4);
+//		Runnable test1 = new Runnable() {
+//			@Override
+//			public void run() {
+//				System.out.println("1");
+//				System.out.println("TEst1 ResULT" + primeNumberGenerator.generatePrimeNumbersInRange(10, 20));
+//				Assert.assertTrue(false);
+//				Assert.assertEquals(primeNumberGenerator.generatePrimeNumbersInRange(10, 15),
+//						new ArrayList<Integer>(Arrays.asList(11, 13)));
+//			}
+//		};
+//
+//		Runnable test2 = new Runnable() {
+//			@Override
+//			public void run() {
+//				System.out.println("2");
+//				System.out.println("TEst2 ResULT" + primeNumberGenerator.generatePrimeNumbersInRange(0, 15));
+//				Assert.assertEquals(primeNumberGenerator.generatePrimeNumbersInRange(0, 15),
+//						new ArrayList<Integer>(Arrays.asList(2, 3, 5, 7, 11, 13)));
+//			}
+//		};
+//		Runnable test3 = new Runnable() {
+//			@Override
+//			public void run() {
+//				System.out.println("3");
+//				Assert.assertEquals(primeNumberGenerator.generatePrimeNumbersInRange(15, 15), new ArrayList<Integer>());
+//			}
+//		};
+//
+//		Runnable test4 = new Runnable() {
+//			@Override
+//			public void run() {
+//				System.out.println("4");
+//				Assert.assertEquals(primeNumberGenerator.generatePrimeNumbersInRange(0, 0), new ArrayList<Integer>());
+//			}
+//		};
+//		threadPool.submit(test1);
+//		threadPool.submit(test3);
+//		threadPool.submit(test4);
+//		threadPool.submit(test2);
+//
+//		try {
+//			Thread.sleep(5000);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		threadPool.shutdown();
+//	}
+//
+//	@Test
+//	public void testNegativeNumbersInRange() {
+//		Assert.assertEquals(new ArrayList<Integer>(), primeNumberGenerator.generatePrimeNumbersInRange(0, 1));
+//		Assert.assertEquals(new ArrayList<Integer>(Arrays.asList(2, 3, 5, 7)),
+//				primeNumberGenerator.generatePrimeNumbersInRange(10, -10));
+//		Assert.assertEquals(new ArrayList<Integer>(Arrays.asList(2, 3, 5, 7)),
+//				primeNumberGenerator.generatePrimeNumbersInRange(-10, 10));
+//		Assert.assertEquals(new ArrayList<Integer>(), primeNumberGenerator.generatePrimeNumbersInRange(-15, -15));
+//		Assert.assertEquals(new ArrayList<Integer>(), primeNumberGenerator.generatePrimeNumbersInRange(-10, -30));
+//		Assert.assertEquals(primeNumberGenerator.generatePrimeNumbersInRange(-30, -3), new ArrayList<Integer>());
+//	}
+//
+//	@Test
+//	public void testNumbersTillEndingNumber() {
+//		Assert.assertEquals(primeNumberGenerator.generatePrimeNumbersTillEndingNumber(-10), new ArrayList<Integer>());
+//		Assert.assertEquals(primeNumberGenerator.generatePrimeNumbersTillEndingNumber(0), new ArrayList<Integer>());
+//		Assert.assertEquals(primeNumberGenerator.generatePrimeNumbersTillEndingNumber(10),
+//				new ArrayList<Integer>(Arrays.asList(2, 3, 5, 7)));
+//		Assert.assertEquals(primeNumberGenerator.generatePrimeNumbersTillEndingNumber(2), new ArrayList<Integer>());
+//	}
+
+//	@Test
+//	public void testSpeed() {
+//		long startingTime = System.nanoTime();
+//		for (int i = 0; i < 10; i++) {
+//			primeNumberGenerator.generatePrimeNumbersTillEndingNumber(1000000);
+//			primeNumberGenerator.generatePrimeNumbersTillEndingNumber(5673356);
+//			primeNumberGenerator.generatePrimeNumbersTillEndingNumber(100);
+//			primeNumberGenerator.generatePrimeNumbersTillEndingNumber(10000);
+//
+//		}
+//		long endingTime = System.nanoTime();
+//		System.out.println("normal Way :" + (endingTime - startingTime) / 1000000000);
+//	}
+
 	@Test
-	public void testPositiveNumbersInRange() {
-		ExecutorService threadPool = Executors.newFixedThreadPool(4);
-		System.out.println("-------"+primeNumberGenerator.generatePrimeNumbersInRange(10, 30));
-
-		Runnable test1 = new Runnable() {
-			@Override
-			public void run() {
-				System.out.println("1");
-				System.out.println("TEst1 ResULT" + primeNumberGenerator.generatePrimeNumbersInRange(10, 15));
-				Assert.assertTrue(false);
-				Assert.assertEquals(primeNumberGenerator.generatePrimeNumbersInRange(10, 15),
-						new ArrayList<Integer>(Arrays.asList(11, 13)));
-			}
-		};
-
-		Runnable test2 = new Runnable() {
-			@Override
-			public void run() {
-				System.out.println("2");
-				System.out.println("TEst2 ResULT" + primeNumberGenerator.generatePrimeNumbersInRange(0, 15));
-				Assert.assertEquals(primeNumberGenerator.generatePrimeNumbersInRange(0, 15),
-						new ArrayList<Integer>(Arrays.asList(2, 3, 5, 7, 11, 13)));
-			}
-		};
-		Runnable test3 = new Runnable() {
-			@Override
-			public void run() {
-				System.out.println("3");
-				Assert.assertEquals(primeNumberGenerator.generatePrimeNumbersInRange(15, 15), new ArrayList<Integer>());
-			}
-		};
-
-		Runnable test4 = new Runnable() {
-			@Override
-			public void run() {
-				System.out.println("4");
-				Assert.assertEquals(primeNumberGenerator.generatePrimeNumbersInRange(0, 0), new ArrayList<Integer>());
-			}
-		};
-		threadPool.submit(test1);
-		threadPool.submit(test3);
-		threadPool.submit(test4);
-		threadPool.submit(test2);
-
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		threadPool.shutdown();
+	public void testPrimesUsingSieves() {
+//		System.out.println("####" + primeNumberGenerator.generatePrimeNumbersBySeive(25));
+		Assert.assertEquals(new ArrayList<Integer>(Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23)),
+				primeNumberGenerator.generatePrimeNumbersBySeive(25));
+		//Assert.assertEquals(new ArrayList<Integer>(), primeNumberGenerator.generatePrimeNumbersBySeive(2));
 	}
 
-	@Test
-	public void testNegativeNumbersInRange() {
-		Assert.assertEquals(primeNumberGenerator.generatePrimeNumbersInRange(10, -10),
-				new ArrayList<Integer>(Arrays.asList(2, 3, 5, 7)));
-		Assert.assertEquals(primeNumberGenerator.generatePrimeNumbersInRange(-10, 10),
-				new ArrayList<Integer>(Arrays.asList(2, 3, 5, 7)));
-		Assert.assertEquals(primeNumberGenerator.generatePrimeNumbersInRange(-15, -15), new ArrayList<Integer>());
-		Assert.assertEquals(primeNumberGenerator.generatePrimeNumbersInRange(-10, -30), new ArrayList<Integer>());
-		Assert.assertEquals(primeNumberGenerator.generatePrimeNumbersInRange(-30, -3), new ArrayList<Integer>());
-	}
+//	@Test
+//	public void testSpeedSeive() {
+//		long startingTime = System.nanoTime();
+//		for (int i = 0; i < 10; i++) {
+//			primeNumberGenerator.generatePrimeNumbersBySeive(1000000);
+//			primeNumberGenerator.generatePrimeNumbersBySeive(5673356);
+//			primeNumberGenerator.generatePrimeNumbersBySeive(100);
+//			primeNumberGenerator.generatePrimeNumbersBySeive(10000);
+//		}
+//		long endingTime = System.nanoTime();
+//		System.out.println("Seive Way :" + (endingTime - startingTime) / 1000000000);
+//	}
 }
